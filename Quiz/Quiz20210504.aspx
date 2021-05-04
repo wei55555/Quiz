c127366a1a96a3b2d235ab9a1ef105836bd7c855
昱瑋 李 <107111226@mail.oit.edu.tw>
2021-05-04 16:30:01 +0800

    123

--- Quiz/Quiz20210504.aspx
+++ Quiz/Quiz20210504.aspx
@@ -10,6 +10,8 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:TextBox ID="tb_Msg" runat="server"></asp:TextBox>
+            <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ErrorMessage="格式錯誤" ValidationExpression="^abc\d*cba$" ControlToValidate="tb_Msg"></asp:RegularExpressionValidator>
         </div>
     </form>
 </body>
--- Quiz/Quiz20210504.aspx.cs
+++ Quiz/Quiz20210504.aspx.cs
@@ -5,10 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Quiz {
-    public partial class Quiz20210504 : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
-
+namespace Quiz
+{
+    public partial class Quiz20210504 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
         }
     }
 }
